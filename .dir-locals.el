4f0abeea20cad16000abde2195b3317b668e410b
Andrea Cardaci <cyrus.and@gmail.com>
2019-03-19 12:51:41 +0100

    Use custom local dictionary

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((ispell-local-dictionary . "english")
+         (eval . (setq ispell-personal-dictionary
+                       (concat (locate-dominating-file default-directory ".dir-locals.el")
+                               "_private/.dictionary"))))))
